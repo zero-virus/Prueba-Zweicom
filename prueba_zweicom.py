68588dd438cafda31f6a4bfd9eb6e20b684cdfb6
CCastil <cristiancastillo.rodriguez@gmail.com>
2018-11-29 15:50:59 +0000

    Upload app

---
+++ prueba_zweicom.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/post', methods=['POST'])
+def post():
+	print(request.is_json)
+	content = request.get_json()
+	ant = 0
+	pres = 1
+	for x in range(content['n']-1):
+		act = ant + pres
+		ant = pres
+		pres = act
+	print(ant)
+	print(pres)
+	return 'JSON posted'
+
+app.run(host='0.0.0.0', port=5000)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
